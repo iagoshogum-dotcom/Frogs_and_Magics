e42f48d503bdda9a8b802479922ea1c8f9742039
iagoshogum-dotcom <iagoshogum@gmail.com>
2026-08-18 12:00:18 -0300

    sapos 3

--- src/main/java/com/escobar/RPG/controller/PersonController.java
+++ src/main/java/com/escobar/RPG/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.escobar.RPG.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -10,10 +11,29 @@ public class PersonController {
     @FXML
     private MediaView mediaViewPerson;
 
+    @FXML
+    private ImageView sapoWarriorSelection;
+
+    @FXML
+    private ImageView sapoWarriorSelection2;
+
+    @FXML
+    private ImageView PersonStatusWarrior;
+
+    @FXML
+    private ImageView warriorHistoria;
+
+
     private MediaPlayer mediaPlayer2;
+
+
 
     @FXML
     public void initialize() {
+        sapoWarriorSelection2.setVisible(false);
+        PersonStatusWarrior.setVisible(false);
+        warriorHistoria.setVisible(false);
+
         String caminho = getClass()
                 .getResource("/com/escobar/RPG/videos/videoFundoSelecao.mp4")
                 .toExternalForm();
@@ -30,4 +50,24 @@ public class PersonController {
 
 
     }
+
+    @FXML
+    public void aoVerWarrior(){
+        sapoWarriorSelection.setVisible(false);
+        sapoWarriorSelection2.setVisible(true);
+        PersonStatusWarrior.setVisible(true);
+        warriorHistoria.setVisible(true);
+
     }
+    @FXML
+    public void aoDesVerWarrior(){
+        sapoWarriorSelection.setVisible(true);
+        sapoWarriorSelection2.setVisible(false);
+        PersonStatusWarrior.setVisible(false);
+        warriorHistoria.setVisible(false);
+
+    }
+
+
+
+}
